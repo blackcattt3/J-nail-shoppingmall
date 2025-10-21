8b75312f2d27c5f8c065c5a012adcca5f2059c7e
Jaehee Roh <snowcat3b@gmail.com>
2025-10-22 01:27:38 +0900

    carousel finish

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -2,40 +2,35 @@ import React from 'react'
 import './HomePage.css';
 import Carousel from 'react-bootstrap/Carousel';
 import ExampleCarouselImage from '../components/ExampleCarouselImage';
+import mainFontImg from '../assets/img/main_font.png'
 
 const HomePage = ({productList}) => {
 
   return (
     <div className='homepage'>
-      <h1>HomePage</h1>
-      <img className='product-img' src={productList?.[0]?.img}/>
-        <Carousel>
-            <Carousel.Item interval={3000} pause='hover'>
+      {/* <h1>HomePage</h1>
+      <img className='product-img' src={productList?.[0]?.img}/> */}
+        <Carousel
+            prevIcon={<span className="custom-prev">❮</span>}
+            nextIcon={<span className="custom-next">❯</span>}>
+            <Carousel.Item interval={2000} pause='hover'>
                 <div className="carousel-item-wrapper">
                     <ExampleCarouselImage/>
                 </div>
-                <Carousel.Caption>
-                <h3>First slide label</h3>
-                </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item interval={3000} pause='hover'>
+            <Carousel.Item interval={2000} pause='hover'>
                 <div className="carousel-item-wrapper">
                     <ExampleCarouselImage/>
                 </div>
-                <Carousel.Caption>
-                <h3>Second slide label</h3>
-                </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item pause='hover'>
+            <Carousel.Item interval={2000} pause='hover'>
                 <div className="carousel-item-wrapper">
                     <ExampleCarouselImage/>
                 </div>
-                <Carousel.Caption>
-                <h3>Third slide label</h3>
-                </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
-    <div className='homepage-line'></div>
+        <span className='section-divider'></span>
+        <img className='product-img' src={mainFontImg}/>
     </div>
   )
 }
